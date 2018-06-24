b9e4d5485f4e7b79ff7912e5dad2d1350d5ed3de
Yoshikazu Oota <atticatticattic@gmail.com>
2018-06-24 21:02:31 +0900

    音声データのプロット処理追記

--- scripts/extract_acoustic_feature.py
+++ scripts/extract_acoustic_feature.py
@@ -9,6 +9,11 @@ from pprint import pprint
 
 import numpy
 
+import os
+import pyworld
+import matplotlib.pyplot as plot
+import librosa.display
+
 from become_yukarin.acoustic_converter import AcousticConverter
 from become_yukarin.config.config import create_from_json as create_config
 from become_yukarin.data_struct import AcousticFeature
@@ -47,6 +52,119 @@ parser.add_argument('--enable_overwrite', action='store_true')
 arguments = parser.parse_args()
 
 pprint(dir(arguments))
+
+def adjustment_len(arry1, arry2):
+    #arry1 = replace_np_array(array1)
+    #arry2 = replace_np_array(array2)
+
+    o1_l = arry1.size
+    o2_l = arry2.size
+    if o1_l > o2_l:
+        ex_l = o1_l - o2_l
+        for i in range(ex_l):
+            arry2 = numpy.append(arry2, 0.0)
+
+    elif o2_l > o1_l:
+        ex_l = o2_l - o1_l
+        for i in range(ex_l):
+            arry1 = numpy.append(arry1 , 0.0)
+
+    return arry1, arry2
+
+
+def replace_np_array(in_array):
+    out = numpy.empty(0)
+    for i in range(len(in_array)):
+        out = numpy.append(out, in_array[i][0])
+
+    return out
+
+
+def plot_acoustic_feature(wave1, wave2, f1, f2, rate, write_path1):
+
+    row_wave1, row_wave2 = adjustment_len(wave1, wave2)
+    f1_f0, f2_f0 = adjustment_len(replace_np_array(f1.f0), replace_np_array(f2.f0))
+    f1_spectrogram, f2_spectrogram = adjustment_len(replace_np_array(f1.spectrogram), replace_np_array(f2.spectrogram))
+    f1_aperiodicity, f2_aperiodicity = adjustment_len(replace_np_array(f1.aperiodicity), replace_np_array(f2.aperiodicity))
+    f1_voiced, f2_voiced = adjustment_len(replace_np_array(f1.voiced), replace_np_array(f2.voiced))
+
+    plot.figure(figsize=(26, 13))
+    plot.subplot(4,3,1)
+    plot.title('origin')
+    librosa.display.waveplot(row_wave1, sr=rate)
+
+    plot.subplot(4,3,2)
+    plot.title('f0')
+    librosa.display.waveplot(f1_f0, sr=rate)
+
+    plot.subplot(4,3,3)
+    plot.title('aperiodicity')
+    librosa.display.waveplot(f1_aperiodicity, sr=rate)
+
+    plot.subplot(4,3,4)
+    plot.title('voiced')
+    librosa.display.waveplot(f1_voiced, sr=rate)
+
+    plot.subplot(4,3,5)
+    plot.title('spectrogram')
+    librosa.display.waveplot(f1_spectrogram, sr=rate)
+
+    plot.subplot(4,3,6)
+    plot.specgram(row_wave1,Fs = rate)
+
+    plot.subplot(4,3,7)
+    plot.title('origin')
+    librosa.display.waveplot(row_wave2, sr=rate)
+
+    plot.subplot(4,3,8)
+    plot.title('f0')
+    librosa.display.waveplot(f2_f0, sr=rate)
+
+    plot.subplot(4,3,9)
+    plot.title('aperiodicity')
+    librosa.display.waveplot(f2_aperiodicity, sr=rate)
+
+    plot.subplot(4,3,10)
+    plot.title('voiced')
+    librosa.display.waveplot(f2_voiced, sr=rate)
+
+    plot.subplot(4,3,11)
+    plot.title('spectrogram')
+    librosa.display.waveplot(f2_spectrogram, sr=rate)
+
+    plot.subplot(4,3,12)
+    plot.specgram(row_wave2,Fs = rate)
+
+    plot.savefig(str(write_path1))
+    plot.close()
+
+
+
+def plot_acoustic_feature_for_check(wave1, wave2, f1, f2, rate, write_path1):
+
+    row_wave1, row_wave2 = adjustment_len(wave1, wave2)
+    f1_f0, f2_f0 = adjustment_len(replace_np_array(f1.f0), replace_np_array(f2.f0))
+
+    plot.figure(figsize=(26, 13))
+    plot.subplot(2,2,1)
+    plot.title('origin')
+    librosa.display.waveplot(row_wave1, sr=rate)
+
+    plot.subplot(2,2,2)
+    plot.title('f0')
+    librosa.display.waveplot(f1_f0, sr=rate)
+
+    plot.subplot(2,2,3)
+    plot.title('origin')
+    librosa.display.waveplot(row_wave2, sr=rate)
+
+    plot.subplot(2,2,4)
+    plot.title('f0')
+    librosa.display.waveplot(f2_f0, sr=rate)
+
+    plot.savefig(str(write_path1))
+    plot.close()
+
 
 pre_convert = arguments.pre_converter1_config is not None
 if pre_convert:
@@ -125,6 +243,26 @@ def generate_feature(path1, path2):
         f1.validate()
         f2.validate()
 
+        wave1 = pyworld.synthesize(
+            f0=numpy.array(f0_1.ravel(), dtype='float64'),
+            spectrogram=numpy.array(spectrogram_1, dtype='float64'),
+            aperiodicity=numpy.array(aperiodicity_1, dtype='float64'),
+            fs=arguments.sample_rate,
+            frame_period=arguments.frame_period
+        )
+
+        wave2 = pyworld.synthesize(
+            f0=numpy.array(f0_2.ravel(), dtype='float64'),
+            spectrogram=numpy.array(spectrogram_2, dtype='float64'),
+            aperiodicity=numpy.array(aperiodicity_2, dtype='float64'),
+            fs=arguments.sample_rate,
+            frame_period=arguments.frame_period
+        )
+
+        out_p1 = Path(str(arguments.output1_directory) + "_ajst_plot", path1.stem)
+        plot_acoustic_feature(wave1, wave2, f1, f2, arguments.sample_rate, out_p1)
+        #  plot_acoustic_feature_for_check(wave1, wave2, f1, f2, arguments.sample_rate, out_p1)
+
     # save
     acoustic_feature_save_process = AcousticFeatureSaveProcess(validate=True, ignore=arguments.ignore_feature)
     acoustic_feature_save_process({'path': out1, 'feature': f1})
@@ -195,8 +333,12 @@ def main():
     arguments.output1_directory.mkdir(exist_ok=True)
     arguments.output2_directory.mkdir(exist_ok=True)
 
-    pool = multiprocessing.Pool()
-    pool.starmap(generate_feature, zip(paths1, paths2), chunksize=16)
+    #pool = multiprocessing.Pool()
+    #pool.starmap(generate_feature, zip(paths1, paths2), chunksize=16)
+
+    for i in range(len(paths1)):
+        generate_feature(paths1[i], paths2[i])
+
 
     generate_mean_var(arguments.output1_directory)
     generate_mean_var(arguments.output2_directory)
